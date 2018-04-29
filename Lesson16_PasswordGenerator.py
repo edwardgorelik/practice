5d3d5636bfa7b9b87e87a1bf1d6a579c75aec19a
edwardgorelik <e.gorelik11@gmail.com>
2018-04-29 01:34:53 -0400

    updated, working

--- Lesson16_PasswordGenerator.py
+++ Lesson16_PasswordGenerator.py
@@ -1,15 +1,18 @@
 import random
 def createPassword(strength):
-    password = []
     list = ["big", "small", "happy", "dog", "cat", "animal", "good", "hello", "i", "we", "meow"]
+    lower = [chr(x) for x in range(ord('a'), ord('z')+1)]
+    upper = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+    number = range(0,9)
+    special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "<", ">", ",", ".", "/", "?", ";", ":", "|", "\\", "}", "\]", "\{", "[", "~", "`", "}" ]
     if strength == "yes":
-        password.append(random.sample("abcdefghijklmnopqrstuvwxyz", random.randint(1,5)))
-        password.append(random.sample("ABCDEFGHIJKLMNOPQRSTUVWXYZ", random.randint(1,5)))
-        password.append(random.sample("1234567890", random.randint(1,3)))
-        password.append(random.sample("!@#$%^&*()<>,./?", random.randint(1,2)))
+        lowers = random.sample(lower, random.randint(1,5))
+        uppers = random.sample(upper, random.randint(1,5))
+        numbers = random.sample(number, random.randint(1,3))
+        specials = random.sample(special, random.randint(1,2))
+        password = lowers + uppers + numbers + specials
         random.shuffle(password)
-        password = ''.join(''.join(elems) for elems in password)
-        print(type(password))
+        password = ''.join(''.join(str(elems) for elems in password))
     else:
         password.append(random.sample(list,random.randint(1,2)))
     return password
